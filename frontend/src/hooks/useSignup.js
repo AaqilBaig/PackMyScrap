7cedc7554d70114fd44b4622781b5e0b9032b802
AaqilBaig <135568009+AaqilBaig@users.noreply.github.com>
2024-07-07 15:33:52 +0530

    Update useSignup.js

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -4,13 +4,13 @@ import { useAuthContext } from './useAuthContext'
 export const useSignup = () => {
   const [error, setError] = useState(null)
   const [isLoading, setIsLoading] = useState(null)
-  const { dispatch } = useAuthContext()
+  const { dispatch,url } = useAuthContext()
 
   const signup = async (email, password) => {
     setIsLoading(true)
     setError(null)
 
-    const response = await fetch('/auth/signup', {
+    const response = await fetch(url + '/auth/signup', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ email, password })
